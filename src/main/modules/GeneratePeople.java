cb710308183525b076fff8780b57a91b58971fa3
Lucifer-is-my-pet <dasha1994@list.ru>
2019-03-23 21:37:22 +0500

    и здесь закрыть

--- src/main/modules/GeneratePeople.java
+++ src/main/modules/GeneratePeople.java
@@ -6,36 +6,39 @@ import java.util.*;
 
 class GeneratePeople {
 
-    public static void main(String[]args) {
-        final String MALE = "Муж";
-        final String FEMALE = "Жен";
-        final String RESOURCES_PATH = "." + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator;
-        final String OUTPUT_PATH = "src" + File.separator + "main" + File.separator + "output" + File.separator;
+    private final static String MALE = "Муж";
+    private final static String FEMALE = "Жен";
+    private final static String RESOURCES_PATH = new StringBuilder().append(".").append(File.separator).append("src")
+            .append(File.separator).append("main").append(File.separator).append("resources").append(File.separator).toString();
+    private final static String OUTPUT_PATH = new StringBuilder().append("src").append(File.separator).append("main")
+            .append(File.separator).append("output").append(File.separator).toString();
 
-        String[] countries = null;
-        String[] districts = null;
-        String[] cities = null;
-        String[] streets = null;
-        Map<String, String[]> names = new HashMap<>();
-        Map<String, String[]> surnames = new HashMap<>();
-        Map<String, String[]> patronNames = new HashMap<>();
+    private static String[] countries = null;
+    private static String[] districts = null;
+    private static String[] cities = null;
+    private static String[] streets = null;
+    private static Map<String, String[]> names = new HashMap<>();
+    private static Map<String, String[]> surnames = new HashMap<>();
+    private static Map<String, String[]> patronNames = new HashMap<>();
+
+    private final static String[] SHEETS_NAMES = {"Люди"};
+    private final static String[] COLUMNS_NAMES = {"Имя", "Фамилия", "Отчество/Среднее имя", "Возраст", "Пол", "Дата рождения", "ИНН",
+            "Почтовый индекс", "Страна", "Область", "Город", "Улица", "Дом", "Квартира"};
+
+    public static void main(String[]args) {
 
         FileReaderToArray readerToArray = new FileReaderToArray();
 
-        try {
-            countries = readerToArray.readLines(RESOURCES_PATH + "Countries.txt");
-            districts = readerToArray.readLines(RESOURCES_PATH + "Districts.txt");
-            cities = readerToArray.readLines(RESOURCES_PATH + "Cities.txt");
-            streets = readerToArray.readLines(RESOURCES_PATH + "Streets.txt");
-            names.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Names_m.txt"));
-            names.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Names_f.txt"));
-            surnames.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Surnames_m.txt"));
-            surnames.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Surnames_f.txt"));
-            patronNames.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Patronymic_m.txt"));
-            patronNames.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Patronymic_f.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        countries = readerToArray.readLines(RESOURCES_PATH + "Countries.txt");
+        districts = readerToArray.readLines(RESOURCES_PATH + "Districts.txt");
+        cities = readerToArray.readLines(RESOURCES_PATH + "Cities.txt");
+        streets = readerToArray.readLines(RESOURCES_PATH + "Streets.txt");
+        names.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Names_m.txt"));
+        names.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Names_f.txt"));
+        surnames.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Surnames_m.txt"));
+        surnames.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Surnames_f.txt"));
+        patronNames.put(MALE, readerToArray.readLines(RESOURCES_PATH + "Patronymic_m.txt"));
+        patronNames.put(FEMALE, readerToArray.readLines(RESOURCES_PATH + "Patronymic_f.txt"));
 
         try {
             String[] sheetsNames = {"Люди"};
