1d0e764e955a68cb39a7ebec45fc0231bd5eecde
Lucifer-is-my-pet <dasha1994@list.ru>
2019-03-26 01:20:22 +0500

    обновления из первой домашки

--- src/main/java/modules/RandomNumber.java
+++ src/main/java/modules/RandomNumber.java
@@ -4,28 +4,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class RandomNumber {
 
-    private int randomNumber;
-    private int start = 0;
+    RandomNumber() {}
 
-    RandomNumber(int start, int bound) {
-        this.start = start;
-
-        this.randomNumber = generateRandomNumber(bound);
+    int generateWithoutStart(int bound) {
+        return ThreadLocalRandom.current().nextInt(0, bound);
     }
 
-    RandomNumber(int bound) {
-        this.randomNumber = generateRandomNumber(bound);
-    }
-
-    private int generateRandomNumber(int bound) {
-        return ThreadLocalRandom.current().nextInt(this.start, bound);
-    }
-
-    int get() {
-        return this.randomNumber;
-    }
-
-    String getString() {
-        return Integer.toString(this.randomNumber);
+    int generateWithStart(int start, int bound) {
+        return ThreadLocalRandom.current().nextInt(start, bound);
     }
 }
